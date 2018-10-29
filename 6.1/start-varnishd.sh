e6b05d742e535e39be997aa32879a5cd293828c6
KataTai <tnt.kata1894@gmail.com>
2018-10-29 12:14:28 +0700

    Update start-varnishd.sh

--- 6.1/start-varnishd.sh
+++ 6.1/start-varnishd.sh
@@ -3,7 +3,7 @@ set -e
 
 exec bash -c \
 	"exec varnishd -n /varnishfs -F \
-	-a ${VARNISH_HOST}:${VARNISH_PORT} \
+	-a 0.0.0.0:6081 \
 	-f ${VCL_CONFIG} \
 	-s malloc,${VARNISH_MEMORY} \
 	${VARNISHD_PARAMS}"
